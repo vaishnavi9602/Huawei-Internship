75a57ef24ffb927d6b102bed3c088fbe7411f7b1
vaishnavi9602 <85212706+vaishnavi9602@users.noreply.github.com>
2022-04-21 18:06:59 +0530

    Add files via upload

---
+++ DatePicker.java
@@ -0,0 +1,68 @@
+MainActivity.java
+package com.example.datepicker;
+
+//package ;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    TextView tvDate;
+    Button btPickDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvDate = findViewById(R.id.tvDate);
+        btPickDate = findViewById(R.id.btPickDate);
+        btPickDate.setOnClickListener(v -> {
+            // Please note that use your package name here
+            com.example.datepicker.DatePicker mDatePickerDialogFragment;
+            mDatePickerDialogFragment = new com.example.datepicker.DatePicker();
+            mDatePickerDialogFragment.show(getSupportFragmentManager(), "DATE PICK");
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+        tvDate.setText(selectedDate);
+    }
+}
+
+DatePicker.java
+package com.example.datepicker;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import java.util.Calendar;
+
+public class DatePicker extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar mCalendar = Calendar.getInstance();
+        int year = mCalendar.get(Calendar.YEAR);
+        int month = mCalendar.get(Calendar.MONTH);
+        int dayOfMonth = mCalendar.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener)
+                getActivity(), year, month, dayOfMonth);
+    }
+}
